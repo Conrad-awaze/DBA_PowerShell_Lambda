40ca1ccc57a6f5eca7c9475ff390a46918fd09f5
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2023-09-21 18:21:37 +0100

    First commit - tested

--- build-layers.ps1
+++ build-layers.ps1
@@ -1,12 +1,32 @@
 $ProgressPreference = 'SilentlyContinue'
 
-$examplesPath   = Split-Path -Path $PSScriptRoot -Parent
-$gitRoot        = Split-Path -Path $examplesPath -Parent
+# $examplesPath   = Split-Path -Path $PSScriptRoot -Parent
 $layersRoot     = Join-Path -Path $PSScriptRoot -ChildPath 'layers'
 
 ####################
 # PwshRuntimeLayer #
 ####################
 $runtimeLayerPath   = Join-Path -Path $layersRoot -ChildPath 'runtimeLayer'
-$runtimeBuildScript = [System.IO.Path]::Combine($gitRoot, 'powershell-runtime', 'build-PwshRuntimeLayer.ps1')
+$runtimeBuildScript = [System.IO.Path]::Combine('powershell-runtime', 'build-PwshRuntimeLayer.ps1')
 & $runtimeBuildScript -PwshArchitecture 'x64' -LayerPath $runtimeLayerPath
+
+#################
+# AWSToolsLayer #
+#################
+$AWS_Modules = @(
+
+    'AWS.Tools.Common','AWS.Tools.AutoScaling','AWS.Tools.CloudFormation','AWS.Tools.CloudWatch','AWS.Tools.CloudWatchLogs','AWS.Tools.DynamoDBv2',
+    'AWS.Tools.EBS','AWS.Tools.EC2','AWS.Tools.EventBridge','AWS.Tools.ElasticLoadBalancing','AWS.Tools.Lambda','AWS.Tools.S3','AWS.Tools.SQS'
+)
+$awsToolsLayerPath = Join-Path -Path $layersRoot -ChildPath 'modulesLayer'
+$awsToolsBuildScript = [System.IO.Path]::Combine('powershell-modules', 'AWSToolsforPowerShell', 'build-AWSToolsLayer.ps1')
+& $awsToolsBuildScript -ModuleList $AWS_Modules -LayerPath $awsToolsLayerPath
+
+########################
+# SAM Template Updates #
+########################
+$samTemplatePath = Join-Path -Path $PSScriptRoot -ChildPath 'template.yml'
+(Get-Content -Path $samTemplatePath -Raw).replace(
+    'ContentUri: ../../powershell-runtime/source', 'ContentUri: ./layers/runtimeLayer').replace(
+    'ContentUri: ../../powershell-modules/AWSToolsforPowerShell/Demo-AWS.Tools/buildlayer', 'ContentUri: ./layers/modulesLayer') | 
+    Set-Content -Path $samTemplatePath -Encoding ascii
---
+++ function/examplehandler.ps1
@@ -0,0 +1,20 @@
+#$VerbosePreference = "continue"
+#$VerbosePreference = "SilentlyContinue"
+Write-Verbose "Run script init tasks before handler"
+Write-Verbose "Importing Modules"
+Import-Module "AWS.Tools.Common",'AWS.Tools.DynamoDBv2','AWS.Tools.Lambda'
+function handler
+{
+    [cmdletbinding()]
+    param(
+        [parameter()]
+        $LambdaInput,
+
+        [parameter()]
+        $LambdaContext
+    )
+    Write-Verbose "Run handler function from script1"
+    Write-Verbose "Function Remaining Time: $($LambdaContext.GetRemainingTimeInMillis())"
+    Get-AWSRegion
+    # Write-Host "PowerShell Version - $($PSVersionTable.PSVersion)"
+}
