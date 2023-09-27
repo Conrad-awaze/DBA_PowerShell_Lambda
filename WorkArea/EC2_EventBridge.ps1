14a6694acb36f0eb89fdc146bcae3a5fd20e47ca
Conrad Gauntlett <conrad.gauntlett@awaze.com>
2023-09-27 12:35:04 +0100

    update

--- WorkArea/EC2_EventBridge.ps1
+++ WorkArea/EC2_EventBridge.ps1
@@ -1,10 +1,6 @@
 Import-Module AWS.Tools.EC2
 
-$Instances =  Get-EC2Instance
-$Instances.Instances
-$Instances.Instances | Where-Object {$_.Tags.Name -eq 'DBA_TestServer'}
-
-Get-Help Get-EC2Instance -Examples
+$EC2 =  Get-EC2Instance
 
 $Inst = (Get-EC2Instance -Filter @{Name="tag:Name";Values="DBA_TestServer"}).Instances
 
